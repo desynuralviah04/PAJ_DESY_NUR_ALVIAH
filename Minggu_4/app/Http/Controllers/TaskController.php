21151a41fa970b5f9a57fbe31df3380656b6449b
B_E31191272_Desy Nur Alviah <80300696+desynuralviah04@users.noreply.github.com>
2024-02-20 08:28:22 +0700

    Create

--- Minggu_4/app/Http/Controllers/TaskController.php
+++ Minggu_4/app/Http/Controllers/TaskController.php
@@ -1,7 +1,8 @@
 <?php
 
 namespace App\Http\Controllers;
-
+use App\Http\Requests\TaskRequest;
+use App\Models\Task;
 use Illuminate\Http\Request;
 
 class TaskController extends Controller
@@ -9,5 +10,24 @@ class TaskController extends Controller
     public function index(){
 
         return view('task.index');
+    }
+
+    public function create(){
+
+        return view('task.create');
+    }
+    public function store (TaskRequest $request) {
+
+        $validatedData = $request->validated();
+    
+        // Pastikan description tersedia sebelum menyimpannya
+        $description = $request->input('description', ''); 
+    
+        $task = new Task();
+        $task->name = $validatedData['name'];
+        $task->description = $description; // Assign description
+        $task->save();
+    
+        return redirect('/task');
     }
 }
--- Minggu_4/routes/web.php
+++ Minggu_4/routes/web.php
@@ -4,6 +4,8 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\TaskController;
 
 Route::get('/task', [TaskController::Class,'index']);
+Route::get('/task/create', [TaskController::Class,'create']);
+Route::post('/task/store', [TaskController::Class,'store']);
 
 Route::get('/', function () {
     return view('welcome');
